005f534d24bbacde5586aa957790dcbcb68d74ce
riomeisya24 <rio.cahclasher@gmail.com>
2024-05-15 23:51:11 +0700

    del

--- koneksi/+connection.php
+++
@@ -1,6 +0,0 @@
-
-
-<?php
-$connection = mysqli_connect("localhost","root","","db_bk");
-?>
-
--- web/pengurus.php
+++
@@ -1,9 +0,0 @@
-	<h3 class="heading-agileinfo">
-				Daftar Pengurus SMK N 10 SURABAYA
-				<span></span>	
-			</h3>
-<div class="well">
-	<img src="slide/pengurus.png" alt="" width="100%" style="border: 3px solid;" class="responsive">
-	<img src="slide/pengurus1.png" alt="" width="100%" style="border: 3px solid;" class="responsive">
-	<img src="slide/pengurus2.png" alt="" width="100%" style="border: 3px solid;" class="responsive">
-</div>
